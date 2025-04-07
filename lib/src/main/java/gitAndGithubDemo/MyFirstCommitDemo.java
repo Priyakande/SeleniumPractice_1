e6279fbbeaf8ae68c2378a563229f21a3b9c0ac4
Dell <Dell@DESKTOP-OM1EPSE>
2025-04-07 18:52:43 +0530

    My Commit

--- lib/src/main/java/gitAndGithubDemo/MyFirstCommitDemo.java
+++ lib/src/main/java/gitAndGithubDemo/MyFirstCommitDemo.java
@@ -1,9 +1,10 @@
 package gitAndGithubDemo;
 
-public class CommitDemo {
+public class MyFirstCommitDemo {
 
 	public static void main(String[] args) {
 		System.out.println("Commit new class");
+
 	}
 
 }
